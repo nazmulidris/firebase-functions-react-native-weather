4bb1f253897fb8b418559ff4b3460abbee275ba6
Nazmul Idris <nazmul@fasterlap.com>
2017-05-29 15:57:09 -0700

    initial commit of firebase functions test project

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,11 @@ var functions = require('firebase-functions');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-let _helloWorld = (req, res) => {
-  res.send("Hola world " + Math.random().toString(36).substring(7));
+let _helloWorld = (req) => {
+  return "Hola world " + Math.random().toString(36).substring(7);
 };
 
 exports.helloWorld = functions.https.onRequest(
   (request, response) => {
-    _helloWorld(req, res);
+    response.send(_helloWorld(request));
   });
