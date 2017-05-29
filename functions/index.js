2310e0c135ce38001e12f7e225c9d82ac3096a95
Nazmul Idris <nazmul@fasterlap.com>
2017-05-29 16:00:59 -0700

    1) bug fix :)

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ var functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 let _helloWorld = (req) => {
-  return "Hola world " + Math.random().toString(36).substring(7);
+  return "Hola world " + Math.random().toString(36).substring(7) + "\n";
 };
 
 exports.helloWorld = functions.https.onRequest(
